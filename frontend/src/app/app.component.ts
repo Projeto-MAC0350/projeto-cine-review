8b6340b9c7cdf94eb4c1cd1aad0a3d6387417657
Francisco Nassif <francisconassif@usp.br>
2025-04-26 20:11:02 -0300

    Adicionando as pastas criadas pelo Angular, junto com um esboço inicial do front end, que contém:
    - Cabeçalho: Logo, Botão de Cadastro, Botão de redirecionamento ao site do CINUSP
    - Frase do site
    - Agenda de filmes
    - Rodapé

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  // experimentando uma função pra conseguir setar as datas da agenda automaticamente,
+  // mas não está usada no html
+  title = 'rev.usp';
+  diasDaSemana: { nome: string, data: string }[] = [];
+
+  constructor(){
+    this.gerarSemana();
+  }
+
+  gerarSemana(){
+    const hoje = new Date();
+    const diaDaSemana = hoje.getDay();
+    const diffSegunda = hoje.getDate() - diaDaSemana + (diaDaSemana == 0 ? -6 : 1);
+    const segunda = new Date(hoje.setDate(diffSegunda));
+
+    const nomeDias = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+
+    for (let i = 0; i < 7; i++){
+      const dia = new Date(segunda);
+      dia.setDate(segunda.getDate() + i);
+      const dataFormatada = this.formatarData(dia);
+      this.diasDaSemana.push({
+        nome: nomeDias[i],
+        data: dataFormatada
+      });
+    }
+  }
+
+  formatarData(data: Date): string {
+    const dia = String(data.getDate()).padStart(2, '0');
+    const mes = String(data.getMonth() + 1).padStart(2, '0');
+    return `${dia}/${mes}`;
+  }
+}
