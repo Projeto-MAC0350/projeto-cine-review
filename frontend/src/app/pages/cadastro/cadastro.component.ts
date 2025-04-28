c2734c42d386a2901968c6ed610f0e426647925c
Francisco Nassif <francisconassif@usp.br>
2025-04-28 19:41:32 -0300

    Criação de página de cadastro separada do login Criação de página de filmes vazia

---
+++ frontend/src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent {
+
+}
---
+++ frontend/src/app/pages/filmes/filmes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-filmes',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './filmes.component.html',
+  styleUrls: ['./filmes.component.css']
+})
+export class FilmesComponent {
+
+}
