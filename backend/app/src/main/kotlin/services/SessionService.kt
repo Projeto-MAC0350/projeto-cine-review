c54112b5050a678b03789aa51711a53b3b66dc79
Francisco Nassif <francisconassif@usp.br>
2025-06-24 18:53:10 -0300

    Compatibilizando SessionService.kt com as alterações anteriores

--- backend/app/src/main/kotlin/services/SessionService.kt
+++ backend/app/src/main/kotlin/services/SessionService.kt
@@ -1,31 +1,28 @@
 package services
 
-import db.tables.Session
+import db.tables.Sessions
 import db.dbo.SessionDbo
 import db.dbo.toSessionDbo
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class SessionService {
 
-    fun getSessions(): List<SessionDbo> {
-        return transaction {
-            Session.selectAll().map { it.toSessionDbo() }
-        }
+    fun getSessions(): List<SessionDbo> = transaction {
+        Sessions.selectAll()
+            .map { it.toSessionDbo() }
     }
 
-    fun getSessionById(id: Int): SessionDbo? {
-        return transaction {
-            Session.select { Session.id eq id }
-                .mapNotNull { it.toSessionDbo() }
-                .singleOrNull()
-        }
+    fun getSessionById(id: Int): SessionDbo? = transaction {
+        Sessions.select { Sessions.id eq id }
+            .mapNotNull { it.toSessionDbo() }
+            .singleOrNull()
     }
 
-    fun getSessionsByMovieId(movieId: Int): List<SessionDbo> {
-        return transaction {
-            Session.select { Session.movieId eq movieId }
-                .map { it.toSessionDbo() }
-        }
+    fun getSessionsByMovieId(movieId: Int): List<SessionDbo> = transaction {
+        Sessions.select { Sessions.movie_id eq movieId }
+            .map { it.toSessionDbo() }
     }
 }
