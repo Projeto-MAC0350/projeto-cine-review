1324815c6ef8ead5b17d3763f893f7744295de28
Francisco Nassif <francisconassif@usp.br>
2025-06-25 10:33:48 -0300

    Compatibilizando MovieDbo com o banco de dados SQL

--- backend/app/src/main/kotlin/db/dbo/MovieDbo.kt
+++ backend/app/src/main/kotlin/db/dbo/MovieDbo.kt
@@ -6,29 +6,41 @@ import org.jetbrains.exposed.sql.ResultRow
 data class MovieDbo(
     val id: Int,
     val title: String,
+    val showLink: String?,
+    val movieLink: String?,
+    val imageLink: String?,
+    val sessions: String?,
+    val movieInfo: String?,
+    val country: String?,
+    val countryType: String?,
+    val year: String?,
+    val yearType: String?,
+    val duration: String?,
+    val durationType: String?,
     val director: String?,
-    val year: Int,
-    val duration: Int,
-    val location: String?,
-    val synopsis: String?,
+    val directorType: String?,
     val exhibitionId: Int,
     val noReviews: Int,
-    val totalRating: Int,
-    val movieLink: String?
+    val totalRating: Int
 )
 
-fun ResultRow.toMovieDbo(): MovieDbo {
-    return MovieDbo(
-        id = this[Movies.id],
-        title = this[Movies.title],
-        director = this[Movies.director],
-        year = this[Movies.year],
-        duration = this[Movies.duration],
-        location = this[Movies.location],
-        synopsis = this[Movies.synopsis],
-        exhibitionId = this[Movies.exhibition_id],
-        noReviews = this[Movies.no_reviews],
-        totalRating = this[Movies.total_rating],
-        movieLink = this[Movies.movie_link]
-    )
-}
+fun ResultRow.toMovieDbo(): MovieDbo = MovieDbo(
+    id            = this[Movies.id],
+    title         = this[Movies.title],
+    showLink      = this[Movies.show_link],
+    movieLink     = this[Movies.movie_link],
+    imageLink     = this[Movies.image_link],
+    sessions      = this[Movies.sessions]?.toString(),
+    movieInfo     = this[Movies.movie_info],
+    country       = this[Movies.country],
+    countryType   = this[Movies.country_type],
+    year          = this[Movies.year],
+    yearType      = this[Movies.year_type],
+    duration      = this[Movies.duration],
+    durationType  = this[Movies.duration_type],
+    director      = this[Movies.director],
+    directorType  = this[Movies.director_type],
+    exhibitionId  = this[Movies.exhibition_id],
+    noReviews     = this[Movies.no_reviews],
+    totalRating   = this[Movies.total_rating]
+)
