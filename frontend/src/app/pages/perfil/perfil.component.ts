eb5ec6744bb262f844b47e730784b9c73ebeeee9
Francisco Nassif <francisconassif@usp.br>
2025-06-25 21:16:44 -0300

    Criando a componente de perfil no front (ainda nao puxa os dados corretamente)

--- frontend/src/app/pages/perfil/perfil.component.ts
+++ frontend/src/app/pages/perfil/perfil.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ProfileService } from '../../services/profile.service';
+import { ReviewService } from '../../services/review.service';
+import { User } from '../../models/user.model';
+import { Review } from '../../models/review.model';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './perfil.component.html',
-  styleUrl: './perfil.component.css'
+  styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
+  user: User | null = null;
+  reviews: Review[] = [];
+  errorUser = '';
+  errorReviews = '';
 
+  constructor(
+    private profileService: ProfileService,
+    private reviewService: ReviewService,
+  ) {}
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe(
+      {
+        next: p => this.user = p,
+        error: () => this.errorUser = 'Erro pra obter o perfil'
+      }
+    );
+    this.reviewService.getMyReviews().subscribe({
+      next: rv => this.reviews = rv,
+      error: () => this.errorReviews = 'Voce nao tem reviews'
+    });
+  }
 }
